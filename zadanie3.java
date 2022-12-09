ccb30b5dea5f90e941b5d61017393e02e8303247
jurekk45 <115872251+jurekk45@users.noreply.github.com>
2022-12-09 21:51:55 +0100

    Update zadanie3.java

--- zadanie3.java
+++ zadanie3.java
@@ -1,16 +1,14 @@
-
-
 public class Main
 {
-	public static void main(String[] args) {
-		
-		
-		Engine engine1 = new Engine(100,"Benzine",2.1);
-		//System.out.println(engine1.engineInformation());
-		
-		Vehicle car1=new Vehicle("Opel","Astra",5,120,engine1);
-		System.out.println(car1.carInformation());
-	}
+    public static void main(String[] args) {
+
+
+        Engine engine1 = new Engine(100,"Benzine",2.1,120);
+        //System.out.println(engine1.engineInformation());
+
+        Vehicle car1=new Vehicle("Opel","Astra",5,engine1);
+        System.out.println(car1.carInformation());
+    }
 }
 
 
@@ -18,26 +16,29 @@ class Engine{
     private int horsePower;
     private String gasType;
     private double engineDisplacement;
-    
-    
-    Engine(int hP,String gT, double eD)
+
+    private int maxSpeed;
+
+
+    Engine(int hP,String gT, double eD,int mS)
     {
         this.horsePower=hP;
         this.gasType=gT;
         this.engineDisplacement=eD;
-        
+        this.maxSpeed=mS;
+
     }
-    
+
     public String engineInformation()
-    {   
-        String data=String.format("Horse Power: %d HP Gas Type: %s Engine Displacement: %.1f",this.horsePower,this.gasType,this.engineDisplacement);
-        
+    {
+        String data=String.format("Horse Power: %d HP Gas Type: %s Engine Displacement: %.1f Max Speed %d km/h",this.horsePower,this.gasType,this.engineDisplacement,this.maxSpeed);
+
         return data;
-        
-        
+
+
     }
-    
-    
+
+
 }
 
 
@@ -45,32 +46,32 @@ class Vehicle
 {
     private String brand;
     private String model;
-    private int maxSpeed;
+
     private int seats;
     private Engine carEngine;
-    
-    Vehicle(String br,String mo,int se,int mS,Engine cE)
+
+    Vehicle(String br,String mo,int se,Engine cE)
     {
         this.brand=br;
         this.model=mo;
         this.seats=se;
-        this.maxSpeed=mS;
+
         this.carEngine=cE;
-        
+
     }
-    
+
     public String carInformation()
     {
         String engine;
         engine=carEngine.engineInformation();
-        String data=String.format("Brand: %s Model: %s Seats: %d Max Speed:  %d km/h %s",this.brand,this.model,this.seats,this.maxSpeed,engine);
-        
+        String data=String.format("Brand: %s Model: %s Seats: %d %s",this.brand,this.model,this.seats,engine);
+
         return data;
-        
+
     }
-    
-    
-    
-    
-    
+
+
+
+
+
 }
