29198f63b626d5864491d618999c32404600b8fb
jurekk45 <115872251+jurekk45@users.noreply.github.com>
2022-12-09 22:39:22 +0100

    Update zadanie4.java

--- zadanie4.java
+++ zadanie4.java
@@ -1,85 +1,123 @@
 
-
 public class Main
 {
-	public static void main(String[] args) {
-		
-		Plant plant1 = new Plant("Cabbage",5,"Field");
-		Plant plant2 = new Plant("Sunflower",11,"Field");
-		
-		System.out.println(plant1.dropSeeds());
-		System.out.println(plant1.germination());
-		
-	    System.out.println(plant2.dropSeeds());
-	    System.out.println(plant2.germination());
-	    plant2.harvest();
-	    System.out.println(plant2.placement());
-	}
+    public static void main(String[] args) {
+
+        Plant plant1 = new Plant("Cabbage",5,"Field_1");
+        Plant plant2 = new Plant("Sunflower",11,"Field_2");
+
+        if(plant2.ripe())
+            {
+                System.out.println("Seeds droped");
+            }
+        else
+            {
+                System.out.println("It is to early!");
+            }
+
+        if(plant1.ripe())
+            {
+                System.out.println("Seeds droped");
+            }
+        else
+            {
+                System.out.println("It is to early!");
+            }
+
+
+
+
+        if(plant1.germinative())
+            {
+                System.out.println("Lets go !");
+            }
+        else
+            {
+                System.out.println("It is to cold! :(");
+            }
+
+        if(plant2.germinative())
+            {
+                System.out.println("Lets go !");
+            }
+        else
+            {
+                System.out.println("It is to cold! :(");
+            }
+
+
+
+        System.out.println(plant2.placement());
+
+        plant2= plant2.harvesting();
+
+        System.out.println(plant2.placement());
+    }
 }
 
 
-interface inter1
+interface PlantsLife
 {
-     String dropSeeds();
-     String germination();
-     Plant harvest();
-     String placement();
-     
-    
+    boolean ripe();
+    boolean germinative();
+    Plant harvesting();
+    String placement();
+
+
 }
 
-class Plant implements inter1
+class Plant implements PlantsLife
 {
     private String name;
     private int month;
     private String place;
-    
-    
+
+
     Plant(String nm,int mt, String pl)
     {
         this.name=nm;
         this.month=mt;
         this.place=pl;
-        
+
     }
-    
-  //  @Override
-  
-    public String dropSeeds()
+
+
+
+    public boolean ripe()
     {
         if(month >=8 && month <=12)
         {
-        return "Seeds droped";
+            return true;
         }
-        
+
         else{
-            return "It is to early!";
+            return false;
         }
     }
-    
-    public String germination()
+
+    public boolean germinative()
     {
         if(month >=4 && month <=6)
         {
-        return "Lets go !";
+            return true ;
         }
-        
+
         else{
-            return "It is to cold! :(";
+            return false ;
         }
-        
+
     }
-    
-    public Plant harvest()
+
+    public Plant harvesting()
     {
         return new Plant(this.name,this.month,"Granary");
     }
-    
-    
+
+
     public String placement()
     {
         return this.place;
     }
-    
-    
+
+
 }
